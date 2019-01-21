7bb20cd9f742edfe30221e2f663297d53a993788
K1614994 <K1614994@users.noreply.github.com>
2019-01-21 17:39:51 +0000

    CharacterController-NotFinished

---
+++ Obscurio/Assets/Scripts/Player.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent (typeof (CharacterController))]
+public class Player : MonoBehaviour {
+
+
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+}
